e4a3f865319c957a11c084bd7f46c576c9939989
unknown <christianodeskaug@hotmail.com>
2018-01-25 22:19:25 +0100

    finpuss

--- hello.go
+++ hello.go
@@ -3,17 +3,8 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
 	fmt.Println("Hello, Christian, test")
 	fmt.Println("Hello, Henrik")
 	fmt.Println("Hello, Kristian")
-=======
-	fmt.Println("Hello, Christian")
->>>>>>> 3f95cbfcb9e154bf40cc9590beca2a7f7b170eea
-}
 
-func main() {
-	fmt.Println("Hello, Henrik")
 }
-func main() {
-        fmt.Println("Hello, Kristian")
