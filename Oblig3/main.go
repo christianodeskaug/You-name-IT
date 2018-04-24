e40e72577eb32eaed10d86d6cd55b54bef07b363
Lindsethen <35816927+Lindsethen@users.noreply.github.com>
2018-04-24 22:22:36 +0200

    Update main.go

--- Oblig3/main.go
+++ Oblig3/main.go
@@ -5,13 +5,16 @@ import (
   "time"
 
 )
-func sayHello(w http.ResponseWriter, r *http.Request) {
+func sayHello(w http.ResponseWriter, r *http.Request)
+{
   message := r.URL.Path
   message = strings.TrimPrefix(message, "/")
   message = "Hello client" + message
   w.Write([]byte(message))
 }
-func main() {
+
+func main() 
+{
   http.HandleFunc("/", sayHello)
   if err := http.ListenAndServe(":8080", nil); err != nil {
     panic(err)
