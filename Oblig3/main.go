fa79cf862ed749d54cc0ba611218f07821fd75ed
christianodeskaug <35773211+christianodeskaug@users.noreply.github.com>
2018-04-24 22:14:38 +0200

    Add files via upload

---
+++ Oblig3/main.go
@@ -0,0 +1,19 @@
+package main
+import (
+  "net/http"
+  "strings"
+  "time"
+
+)
+func sayHello(w http.ResponseWriter, r *http.Request) {
+  message := r.URL.Path
+  message = strings.TrimPrefix(message, "/")
+  message = "Hello client" + message
+  w.Write([]byte(message))
+}
+func main() {
+  http.HandleFunc("/", sayHello)
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    panic(err)
+  }
+}
